912cc980ec0c06a494001d6e7faf925cf2a391ae
craig <craig@nykindale.com>
2024-01-28 19:41:59 +0000

    Adjusted image for alpine's runbox crond. User permissions now working correctly so dovecot (user 1000:1000) can read the emails.

---
+++ src/root/etc/periodic/5min/mbsync.sh
@@ -0,0 +1,1 @@
+mbsync -c /mbsyncrc.conf {NAME}
--- src/root/start.sh
+++ src/root/start.sh
@@ -1,9 +1,17 @@
 #!/bin/sh
 
+# Add our 5 minute periodic folder to run isync
+echo "*/5   *   *   *   *   run-parts /etc/periodic/5min" >> /etc/crontabs/root
+
+# Replace variables in /mbsyncrc.conf
 sed -i "s/{NAME}/${NAME}/g" /mbsyncrc.conf
-sed -i "s/{SERVER}/${SERVER}/g" /mbsyncrc.conf
+sed -i "s/{SERVER}/${SERVER}/g" //mbsyncrc.conf
 sed -i "s/{EMAIL}/${EMAIL}/g" /mbsyncrc.conf
 sed -i "s/{PASSWORD}/${PASSWORD}/g" /mbsyncrc.conf
-sed -i "s/{NAME}/${NAME}/g" /etc/crontabs/root
+
+# Replace variables in /etc/periodic/5min/mbsync.sh
+sed -i "s/{NAME}/${NAME}/g" /etc/periodic/5min/mbsync.sh
+
+#chown -R app /mail_data
 
 crond -f -d 8
